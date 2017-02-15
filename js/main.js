55faf8a1851ac13a39284325f996e537bc024546
Carey Costa <careyxcosta@gmail.com>
2017-02-15 17:11:39 -0500

    h1 changes depending on switch class=on/off

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
 $(document).ready(function() {
 
 // When someone hits the button in the switch, remove the current state class (on/off), and apply the opposite class (on/off)
+
+// Swap the colors of the body's background and text by adding or removing the dark/light class
+
     $('.switch button').on('click', function() {
       if ($('.switch button').hasClass('on')) {
         $('.switch .on').attr('class', 'off');
         $('body').attr('class', 'dark');
+        $('.status').text("It's so bright in here!");
       }
       else {
         $('.switch .off').attr('class', 'on');
         $('body').attr('class', 'light');
+        $('.status').text("Hey, who turned the lights off?");
       }
-  });
-
-// Swap the colors of the body's background and text by adding or removing the dark/light class
-
+    });
 });
