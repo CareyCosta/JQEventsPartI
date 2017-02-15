36cbfe10b4a99fbb5f91af02cd31f007a4d44266
Carey Costa <careyxcosta@gmail.com>
2017-02-15 16:26:19 -0500

    body background switches from light/dark with switch

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
+
+// When someone hits the button in the switch, remove the current state class (on/off), and apply the opposite class (on/off)
     $('.switch button').on('click', function() {
       if ($('.switch button').hasClass('on')) {
-        $('.switch .on').attr('class', 'off')
+        $('.switch .on').attr('class', 'off');
+        $('body').attr('class', 'dark');
       }
       else {
-        $('.switch .off').attr('class', 'on')
+        $('.switch .off').attr('class', 'on');
+        $('body').attr('class', 'light');
       }
   });
+
+// Swap the colors of the body's background and text by adding or removing the dark/light class
+
 });
